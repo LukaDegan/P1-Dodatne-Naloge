3996bee858fb1a36573d136ca10c9891319ba464
LukaDegan <degan.luka@gmail.com>
2024-08-23 19:54:42 +0200

    Initial commit

--- 01_krmilniKonstrukti/01_absolutnaVrednost/AbsolutnaVrednost.java
+++ 01_krmilniKonstrukti/01_absolutnaVrednost/AbsolutnaVrednost.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class AbsolutnaVrednost {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int st = sc.nextInt();
 
         System.out.println(Math.abs(st));
